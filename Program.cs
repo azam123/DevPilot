2734e8dbb84224e8e2bc53f345690a3d6f0138d7
Mohammad Azam Shaikh <azam1200@gmail.com>
2026-09-17 23:12:14 +0530

    Register model conversion service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-using DevPilot.Api;
+using DevPilot.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => c.EnableAnnotations());
+builder.Services.AddSingleton<IModelConversionService, ModelConversionService>();
 
 var app = builder.Build();
 app.UseSwagger();
