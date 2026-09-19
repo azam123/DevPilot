07f3a436bf09608cca85fa7224926638c300f4ec
Mohammad Azam Shaikh <azam1200@gmail.com>
2026-09-19 08:36:49 +0530

    Register validation and global error handling

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using DevPilot.Infrastructure;
 using DevPilot.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,8 +6,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => c.EnableAnnotations());
 builder.Services.AddSingleton<IModelConversionService, ModelConversionService>();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
+app.UseExceptionHandler();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
